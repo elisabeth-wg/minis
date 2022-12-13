15fba890daab7550192bf68ed931da114871c299
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2022-12-12 19:03:39 -0600

    Mini JS 12-12-2022-2

---
+++ mini12-12-2022-2/main.js
@@ -0,0 +1,18 @@
+//Dog years
+//sets human age
+let myAge = 99
+//set math variable for early years
+let earlyYears = 2
+earlyYears *= 10.5
+myAge -= 2
+//set math variable for later years
+let laterYears = myAge
+//accounts for later years being *4 for dogs
+laterYears *= 4
+console.log(earlyYears)
+console.log(laterYears)
+//finalizes human age in dog years
+myAgeInDogYears = earlyYears + laterYears
+const myName = ("Elisabeth".toLowerCase())
+//Sentence to explain share info concisely
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
